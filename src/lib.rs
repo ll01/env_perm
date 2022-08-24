cec2b13041391e63b992a9796700aacbad1bf0b2
ll01 <enti2@hotmail.co.uk>
2022-08-24 10:31:28 +0100

    working on abstracting code to interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,86 +1,77 @@
-//! This crate allows you to permanently set environment variables
-//!
-//! # Examples
-//! ```rust
-//! // Check if DUMMY is set, if not set it to 1
-//! // export DUMMY=1
-//! env_perm::check_or_set("DUMMY", 1).expect("Failed to find or set DUMMY");
-//! // Append $HOME/some/cool/bin to $PATH
-//! // export PATH= "$HOME/some/cool/bin:$PATH"
-//! env_perm::append("PATH", "$HOME/some/cool/bin").expect("Couldn't find PATH");
-//! // Sets a variable without checking if it exists.
-//! // Note you need to use a raw string literal to include ""
-//! // export DUMMY="/something"
-//! env_perm::set("DUMMY", r#""/something""#).expect("Failed to set DUMMY");
-//! ```
-
-use std::io::{self, Write};
-use std::fs::{File, OpenOptions};
-use std::path::PathBuf;
+use dirs;
 use std::env;
 use std::fmt;
-use dirs;
+use std::fs::{File, OpenOptions};
+use std::io::{self, Write};
+use std::path::PathBuf;
 
-/// Checks if a environment variable is set.
-/// If it is then nothing will happen.
-/// If it's not then it will be added
-/// to your profile.
-pub fn check_or_set<T, U>(var: T, value: U) -> io::Result<()>
-where T: fmt::Display + AsRef<std::ffi::OsStr>,
-      U: fmt::Display,
-{
-    env::var(&var)
-        .map(|_|())
-        .or_else(|_| set(var, value))
+struct env_perm {}
+trait environment_operations {
+    pub fn check_or_set<T, U>(var: T, value: U) -> io::Result<()>;
+    pub fn append<T: fmt::Display>(var: T, value: T) -> io::Result<()>;
+    pub fn set<T: fmt::Display, U: fmt::Display>(var: T, value: U) -> io::Result<()>;
 }
+impl environment_operations for env_perm {
+    /// Checks if a environment variable is set.
+    /// If it is then nothing will happen.
+    /// If it's not then it will be added
+    /// to your profile.
+    fn check_or_set<T, U>(var: T, value: U) -> io::Result<()>
+    where
+        T: fmt::Display + AsRef<std::ffi::OsStr>,
+        U: fmt::Display,
+    {
+        env::var(&var).map(|_| ()).or_else(|_| set(var, value))
+    }
 
-/// Appends a value to an environment variable
-/// Useful for appending a value to PATH
-pub fn append<T: fmt::Display>(var: T, value: T) -> io::Result<()> {
-    let mut profile = get_profile()?;
-    writeln!(profile, "\nexport {}=\"{}:${}\"", var, value, var)?;
-    profile.flush()
-}
+    /// Appends a value to an environment variable
+    /// Useful for appending a value to PATH
+    fn append<T: fmt::Display>(var: T, value: T) -> io::Result<()> {
+        let mut profile = get_profile()?;
+        writeln!(profile, "\nexport {}=\"{}:${}\"", var, value, var)?;
+        profile.flush()
+    }
 
-/// Sets an environment variable without checking
-/// if it exists.
-/// If it does you will end up with two
-/// assignments in your profile.
-/// It's recommended to use `check_or_set`
-/// unless you are certain it doesn't exist.
-pub fn set<T: fmt::Display, U: fmt::Display>(var: T, value: U) -> io::Result<()> {
-    let mut profile = get_profile()?;
-    writeln!(profile, "\nexport {}={}", var, value)?;
-    profile.flush()
-}
+    /// Sets an environment variable without checking
+    /// if it exists.
+    /// If it does you will end up with two
+    /// assignments in your profile.
+    /// It's recommended to use `check_or_set`
+    /// unless you are certain it doesn't exist.
+    fn set<T: fmt::Display, U: fmt::Display>(var: T, value: U) -> io::Result<()> {
+        let mut profile = get_profile()?;
+        writeln!(profile, "\nexport {}={}", var, value)?;
+        profile.flush()
+    }
 
-fn get_profile() -> io::Result<File> {
-    dirs::home_dir()
-        .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "No home directory"))
-        .and_then(find_profile)
-}
+    fn get_profile() -> io::Result<File> {
+        dirs::home_dir()
+            .ok_or_else(|| io::Error::new(
+                io::ErrorKind::Other, "No home directory"))
+            .and_then(find_profile)
+    }
 
-#[cfg(target_family = "unix")]
-fn find_profile(mut profile: PathBuf) -> io::Result<File> {
-    profile.push(".bash_profile");
-    let mut oo = OpenOptions::new();
-    oo.append(true)
-        .create(false);
-    oo.open(profile.clone())
-        .or_else(|_|{
-            profile.pop();
-            profile.push(".bash_login");
-            oo.open(profile.clone())
-        })
-        .or_else(|_|{
-            profile.pop();
-            profile.push(".profile");
-            oo.open(profile.clone())
-        })
-        .or_else(|_|{
-            profile.pop();
-            profile.push(".bash_profile");
-            oo.create(true);
-            oo.open(profile.clone())
-        })
+    #[cfg(target_family = "unix")]
+    fn find_profile(mut profile: PathBuf) -> io::Result<File> {
+        profile.push(".bash_profile");
+        let mut oo = OpenOptions::new();
+        oo.append(true).create(false);
+        oo.open(profile.clone())
+            .or_else(|_| {
+                profile.pop();
+                profile.push(".bash_login");
+                oo.open(profile.clone())
+            })
+            .or_else(|_| {
+                profile.pop();
+                profile.push(".profile");
+                oo.open(profile.clone())
+            })
+            .or_else(|_| {
+                profile.pop();
+                profile.push(".bash_profile");
+                oo.create(true);
+                oo.open(profile.clone())
+            })
+    }
 }
---
+++ tests/lib_tests.rs
@@ -0,0 +1,35 @@
+use env_perm;
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// This is a really bad adding function, its purpose is to fail in this
+// example.
+#[allow(dead_code)]
+fn bad_add(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+#[cfg(test)]
+mod lib_tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+
+    #[test]
+    fn test_bad_add() {
+        // This assert would fire and test will fail.
+        // Please note, that private functions can be tested too!
+        assert_eq!(bad_add(1, 2), 3);
+    }
+
+    #[test]
+    fn test_check_or_set() {
+        env_perm::check_or_set("DUMMY", 1).expect("Failed to find or set DUMMY");
+    }
+}
